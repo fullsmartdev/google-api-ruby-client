2e5e2c2ee76495c288c064d8d651a3b9a420a556
HideAKi <hideaki.chain@gmail.com>
2013-11-20 00:22:28 +0100

    Shutdown WEBrick server used in installed app flow

--- lib/google/api_client/auth/file_storage.rb
+++ lib/google/api_client/auth/file_storage.rb
@@ -12,42 +12,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require 'json'
 require 'signet/oauth_2/client'
-require_relative 'storage'
-require_relative 'storages/file_store'
 
 module Google
   class APIClient
-
     ##
-    # DEPRECATED WARNING
-    # This class is deprecated use Storage and FileStore as
-    # mentioned in the samples
-    #
     # Represents cached OAuth 2 tokens stored on local disk in a
     # JSON serialized file. Meant to resemble the serialized format
     # http://google-api-python-client.googlecode.com/hg/docs/epy/oauth2client.file.Storage-class.html
     #
     class FileStorage
+      # @return [String] Path to the credentials file.
+      attr_accessor :path
 
-      attr_accessor :storage,
-                    :path
+      # @return [Signet::OAuth2::Client] Path to the credentials file.
+      attr_reader :authorization
 
+      ##
+      # Initializes the FileStorage object.
+      #
+      # @param [String] path
+      #    Path to the credentials file.
       def initialize(path)
-        Google::ApiClient.logger("DEPRECATED: Please use Storage Class instead.")
         @path = path
-        store = Google::APIClient::FileStore.new(@path)
-        @storage = Google::APIClient::Storage.new(store)
-        @storage.authorize
+        self.load_credentials
       end
 
+      ##
+      # Attempt to read in credentials from the specified file.
       def load_credentials
-        Google::ApiClient.logger("DEPRECATED: Please use Storage Class instead.")
-        storage.authorize
-      end
-
-      def authorization
-        storage.authorization
+        if File.exist? self.path
+          File.open(self.path, 'r') do |file|
+            cached_credentials = JSON.load(file)
+            @authorization = Signet::OAuth2::Client.new(cached_credentials)
+            @authorization.issued_at = Time.at(cached_credentials['issued_at'])
+            if @authorization.expired?
+              @authorization.fetch_access_token!
+              self.write_credentials
+            end
+          end
+        end
       end
 
       ##
@@ -56,9 +61,26 @@ module Google
       # @param [Signet::OAuth2::Client] authorization
       #    Optional authorization instance. If not provided, the authorization
       #    already associated with this instance will be written.
-      def write_credentials(auth=nil)
-        self.authorization = auth unless auth.nil?
-        storage.write_credentials(self.authorization)
+      def write_credentials(authorization=nil)
+        @authorization = authorization unless authorization.nil?
+
+        unless @authorization.refresh_token.nil?
+          hash = {}
+          %w'access_token
+           authorization_uri
+           client_id
+           client_secret
+           expires_in
+           refresh_token
+           token_credential_uri'.each do |var|
+            hash[var] = @authorization.instance_variable_get("@#{var}")
+          end
+          hash['issued_at'] = @authorization.issued_at.to_i
+
+          File.open(self.path, 'w', 0600) do |file|
+            file.write(hash.to_json)
+          end
+        end
       end
     end
   end
